a0838453be80d4c685a9f1a748b75278e6346c99
HamKDaBu11 <khawajahamza35@outlook.com>
2025-01-20 16:34:26 +0500

    feat: set manual

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -14,13 +14,13 @@ const CONFIG = {
     github: {
       display: true, // Display GitHub projects?
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'manual', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: ['HamKDaBu11/tech-assignment'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: ['https://github.com/HamKDaBu11/tech-assignment'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {

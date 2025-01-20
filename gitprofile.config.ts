92a80ae8f522dfc744cb8365e79f58b173b5b628
HamKDaBu11 <khawajahamza35@outlook.com>
2025-01-20 16:27:47 +0500

    feat: correction

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -20,7 +20,7 @@ const CONFIG = {
         limit: 8, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: ['https://github.com/HamKDaBu11/tech-assignment.git'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: ['HamKDaBu11/tech-assignment'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {

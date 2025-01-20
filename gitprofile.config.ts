5554c8ba72a4f334f961ef0f9fdff4b34444d30c
HamKDaBu11 <khawajahamza35@outlook.com>
2025-01-20 16:46:08 +0500

    feat: correction to project name 'gibson'

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['https://github.com/HamKDaBu11/gibson.git'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['HamKDaBu11/gibson'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
